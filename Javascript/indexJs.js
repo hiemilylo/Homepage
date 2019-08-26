28bd3b50c8d67e48b4e031c1d6d054eaa73ce648
Emily Lo <hiemilylo@gmail.com>
2019-08-26 19:44:48 -0400

    Rough draft, wireframe

--- Javascript/indexJs.js
+++ Javascript/indexJs.js
@@ -1,47 +1,42 @@
-// vars
-var num=30;
-var objects=[];
-var raycaster = new THREE.Raycaster();
-var mouse = new THREE.Vector2();
-var light,t;
+// let num=30;
+// let raycaster = new THREE.Raycaster();
+// let mouse = new THREE.Vector2();
+let light, t;
 
-// create camera
-var camera = new THREE.PerspectiveCamera
-( 65, window.innerWidth/window.innerHeight,
-0.1, 1000 );
-camera.position.set(0.0,0.0,5);
-// create a scene
-var scene = new THREE.Scene();
+// Camera
+let camera = new THREE.PerspectiveCamera
+	(65, window.innerWidth/window.innerHeight, 0.1, 1000);
+camera.position.set(0.0, 0.0, 5);
 
-// create renderer
-var renderer = new THREE.WebGLRenderer(
-{antialias:true});
-renderer.setSize( window.innerWidth, window.
-innerHeight );
-document.body.appendChild( renderer.domElement
-);
+// Scene and renderer
+let scene = new THREE.Scene();
+let renderer = new THREE.WebGLRenderer({antialias:true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-//Create a Spot light
-light = new THREE.SpotLight( 0xccddff,.8 );
-light.position.set(0,0,5);
-scene.add( light );
+// Light source
+light = new THREE.SpotLight(0xccddff,.9);
+light.position.set(0, 0, 5);
+scene.add(light);
 
-var animate = function () {
-	requestAnimationFrame( animate );
+// Shape
+let geometry = new THREE.TetrahedronBufferGeometry(2,1);
+let material = new THREE.MeshPhysicalMaterial({
+	wireframe: true
+});
+t = new THREE.Mesh(geometry, material);
+scene.add(t);
+scene.background = new THREE.Color("rgb(10, 10, 10)");
+
+let animate = function () {
+	requestAnimationFrame(animate);
+	t.rotation.y -= 0.005;
+	t.rotation.x -= 0.0025;
+	camera.lookAt(t.position);
 	renderer.render(scene, camera);
 };
 
 // start animation loop
 animate();
 
-// create Tetrahedron
-var geometry = new THREE.
-TetrahedronBufferGeometry(2,0);
-var material = new THREE.MeshPhysicalMaterial
-( { map:texture,   envMap:envMap,
-metalness:1.0,roughness:0.0 });
-t = new THREE.Mesh( geometry, material );
-t.rotation.x=Math.PI/180*-10;
-scene.add( t );
-
-console.log('hi!')
+console.log('1')
