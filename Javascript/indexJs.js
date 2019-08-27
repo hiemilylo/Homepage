f5763051e50cb7f9e437459ae7b2298c81b09c53
Emily Lo <hiemilylo@gmail.com>
2019-08-27 14:27:10 -0400

    More opaque lines

--- Javascript/indexJs.js
+++ Javascript/indexJs.js
@@ -1,42 +1,53 @@
-// let num=30;
-// let raycaster = new THREE.Raycaster();
-// let mouse = new THREE.Vector2();
-let light, t;
+// let num=30
+// let raycaster = new THREE.Raycaster()
+// let mouse = new THREE.Vector2()
+let light, t
+let deltaX = 0.0025
+let deltaY = 0.0025
 
 // Camera
 let camera = new THREE.PerspectiveCamera
-	(65, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.set(0.0, 0.0, 5);
+	(65, window.innerWidth/window.innerHeight, 0.1, 1000)
+camera.position.set(0.0, 0.0, 5)
 
 // Scene and renderer
-let scene = new THREE.Scene();
-let renderer = new THREE.WebGLRenderer({antialias:true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+let scene = new THREE.Scene()
+let renderer = new THREE.WebGLRenderer({antialias: true})
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.getElementById('landing').appendChild(renderer.domElement)
 
 // Light source
-light = new THREE.SpotLight(0xccddff,.9);
-light.position.set(0, 0, 5);
-scene.add(light);
+light = new THREE.SpotLight(0xccddff, 13)
+light.position.set(0, 0, 5)
+scene.add(light)
 
 // Shape
-let geometry = new THREE.TetrahedronBufferGeometry(2,1);
+let geometry = new THREE.TetrahedronBufferGeometry(2,1)
 let material = new THREE.MeshPhysicalMaterial({
 	wireframe: true
-});
-t = new THREE.Mesh(geometry, material);
-scene.add(t);
-scene.background = new THREE.Color("rgb(10, 10, 10)");
+})
+t = new THREE.Mesh(geometry, material)
+scene.add(t)
+scene.background = new THREE.Color("rgb(10, 10, 10)")
 
 let animate = function () {
-	requestAnimationFrame(animate);
-	t.rotation.y -= 0.005;
-	t.rotation.x -= 0.0025;
-	camera.lookAt(t.position);
-	renderer.render(scene, camera);
-};
+	requestAnimationFrame(animate)
+	t.rotation.y += deltaY
+	t.rotation.x += deltaX
+	camera.lookAt(t.position)
+	renderer.render(scene, camera)
+}
 
 // start animation loop
-animate();
+animate()
+
+window.addEventListener('resize', onWindowResize, false)
+
+function onWindowResize(){
+	camera = new THREE.PerspectiveCamera
+			 (65, window.innerWidth/window.innerHeight, 0.1, 1000)
+	camera.position.set(0.0, 0.0, 5)
+    renderer.setSize( window.innerWidth, window.innerHeight )
+}
 
 console.log('1')
