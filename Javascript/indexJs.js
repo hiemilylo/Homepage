39494d0b8ab8727f3bbf068d153870dade4cf73b
Emily Lo <hiemilylo@gmail.com>
2019-08-28 23:11:45 -0400

    Light description, photo positioning

--- Javascript/indexJs.js
+++ Javascript/indexJs.js
@@ -1,9 +1,11 @@
-// let num=30
-// let raycaster = new THREE.Raycaster()
-// let mouse = new THREE.Vector2()
+let numParticles = 0
+let particles = []
+let raycaster = new THREE.Raycaster()
+let mouse = new THREE.Vector2()
 let light, t
 let deltaX = 0.0025
 let deltaY = 0.0025
+let particleDelta = 0.005
 
 // Camera
 let camera = new THREE.PerspectiveCamera
@@ -36,6 +38,40 @@ let animate = function () {
 	t.rotation.x += deltaX
 	camera.lookAt(t.position)
 	renderer.render(scene, camera)
+	for (let i = 0; i < numParticles; i++){
+		let o = particles[i]
+		o.rotation.y += .01
+		if( i % 2 == 0) {
+			o.radians  += particleDelta
+			o.radians2 += particleDelta
+		} else {
+			o.radians -= particleDelta
+			o.radians2 -= particleDelta
+		}
+		o.position.x = (Math.cos(o.radians) * o.distance)
+		o.position.z = (Math.sin(o.radians) * o.distance)
+		o.position.y = (Math.sin(o.radians2) * o.distance*.5)
+	}
+}
+
+// Particles
+for (let i= 0; i < numParticles; i++) {
+	// Mesh
+	let geometry = new THREE.SphereBufferGeometry(.1,6,6)
+	let material = new THREE.MeshPhysicalMaterial({
+		metalness: 1.0
+	})
+	let particle = new THREE.Mesh(geometry, material)
+	// set random position
+	particle.position.set(Math.random() * 100.0 - 50.0, 0.0, Math.random() * -10.0 - 10)
+	var a = new THREE.Vector3(0, 0, 0)
+	var b = particle.position
+	var d = a.distanceTo(b)
+	particle.distance = d
+	particle.radians = Math.random()*360 * Math.PI/180
+	particle.radians2 = Math.random()*360 * Math.PI/180
+	scene.add(particle)
+	particles.push(particle)
 }
 
 // start animation loop
@@ -47,7 +83,7 @@ function onWindowResize(){
 	camera = new THREE.PerspectiveCamera
 			 (65, window.innerWidth/window.innerHeight, 0.1, 1000)
 	camera.position.set(0.0, 0.0, 5)
-    renderer.setSize( window.innerWidth, window.innerHeight )
+    renderer.setSize(window.innerWidth, window.innerHeight)
 }
 
 console.log('1')
